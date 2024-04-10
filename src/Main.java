f0ead7e4a7ff02d7687cdb240fb246a307823e2e
Bigbrainium <regmi.616969@swl.k12.oh.us>
2024-04-09 21:11:12 -0400

    gyatt

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ public class Main {
         int numOfClasses;
         int low = 150;
         int high = -1;
+        String lowestClass = "";
+        String highestClass = "";
         double sum = 0;
         double avg;
 
@@ -28,10 +30,12 @@ public class Main {
 
             if (classGrades.get(subject) > high) {
                 high = classGrades.get(subject);
+                highestClass = subject;
             }
 
             if (classGrades.get(subject) < low) {
                 low = classGrades.get(subject);
+                lowestClass = subject;
             }
 
             sum += classGrades.get(subject);
@@ -41,8 +45,8 @@ public class Main {
 
         System.out.println(" ");
         System.out.printf("\n%-20s %5d", "Number of Classes:", classGrades.size());
-        System.out.printf("\n%-20s %5d", "Lowest Grade:", low);
-        System.out.printf("\n%-20s %5d", "Highest Grade:", high);
+        System.out.printf("\n%-35s %5d", "Lowest Grade (" + lowestClass + "):", low);
+        System.out.printf("\n%-35s %5d", "Highest Grade (" + highestClass + "):", high);
         System.out.printf("\n%-20s %5f", "Average Grade:", avg);
     }
 }
